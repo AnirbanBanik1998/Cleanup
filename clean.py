643a7074e4acd18e3d6019330edb89ca1e9139ab
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-12-21 13:59:46 +0530

    Updated main cleaner script

--- clean.py
+++ clean.py
@@ -1,32 +1,39 @@
 import os, time, subprocess
+import getpass as gp
 from threading import Lock
 lock=Lock()
+username=gp.getuser()
 class Clean:
 	
-	def __init__(self, days=20, rec=False):
-		self.rec=rec
+	def __init__(self, days=60):
 		self.days=days
 		self.start=time.time()
+		
 	def current(self, p):
-		for self.i in os.listdir(p):
-			if not os.path.isdir(self.i):
-				print(self.i)
-				self.d=(self.start-os.stat(p).st_atime)/(3600*24)
-				self.e=(self.start-os.stat(p).st_mtime)/(3600*24)
-				if int(self.d)>self.days or int(self.e)>self.days:
-					self.a=input("Delete "+self.i+"? Yes or No")
-					type(self.a)
-					if self.a=="Yes":
-						os.system("mv " +p+"/"+self.i+" /home/anirban/.local/share/Trash")
+		print("In path: "+p)
+		for i in os.listdir(p):
+			if not os.path.isdir(i):
+				print(i)
+				d=(self.start-os.stat(p).st_atime)/(3600*24)
+				e=(self.start-os.stat(p).st_mtime)/(3600*24)
+				if int(d)>self.days or int(e)>self.days:
+					a=input("Delete "+i+"? Yes or No: ")
+					type(a)
+					if a=="Yes":
+						os.system("mv -f " +p+"/"+i+" /home/"+username+"/.local/share/Trash/files/")
+						
 	def all(self, p):
-		self.j=subprocess.check_output(["find", p, "-type", "d", "-print"])
-		self.j=self.j.decode("utf-8")
-		self.line=self.j.split("\n")
-		for self.k in self.line:
-			print(self.k)
+		j=subprocess.check_output(["find", p, "-type", "d", "-print"])
+		j=j.decode("utf-8")
+		line=j.split("\n")
+		for k in line:
 			with lock:
 				try:
-					self.current(self.k)
-				except Exception as e:
-					print(e)
-
+					self.current(k)
+				except KeyboardInterrupt:
+					inp=input("\nPress C to Continue to next directory or B to Break the loop: ")
+					type(inp)
+					if inp == "C":
+						continue
+					elif inp == "B":
+						break  
